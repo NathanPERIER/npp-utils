f75ef30a0adef45dbf9344f724f39e1ac8eee9ab
NathanPERIER <periernathan2000@gmail.com>
2024-08-19 00:55:55 +0200

    Added unordered generic string map

--- lib/npp/typing/map.hh
+++ lib/npp/typing/map.hh
@@ -3,11 +3,21 @@
 
 #include <map>
 #include <string>
+#include <unordered_map>
 
 
 namespace npp {
 
+struct string_hasher {
+    using is_transparent = void;
+    size_t operator()(const std::string& s) const { return std::hash<std::string>{}(s);      }
+    size_t operator()(std::string_view s)   const { return std::hash<std::string_view>{}(s); }
+};
+
 template <typename T>
 using string_map = std::map<std::string, T, std::less<>>;
+
+template <typename T>
+using string_umap = std::unordered_map<std::string, T, string_hasher, std::equal_to<>>;
 
 } // namespace npp
