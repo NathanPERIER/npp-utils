61b001e1f0a0acbe313bd20c8617ea936ae1c198
NathanPERIER <periernathan2000@gmail.com>
2024-08-18 14:20:54 +0200

    Added method to compute the name of an enum member at compile time

--- lib/npp/type_name.hh
+++ lib/npp/type_name.hh
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <string_view>
+#include <type_traits>
 
 
 namespace npp {
@@ -27,6 +28,34 @@ constexpr auto type_name() {
 #error "Unsupported compiler"
 #endif
     name.remove_prefix(prefix.size());
+    name.remove_suffix(suffix.size());
+    return name;
+}
+
+
+template <typename E, E Val>
+requires(std::is_enum_v<E>)
+constexpr auto enum_name() {
+    constexpr std::string_view enum_type_name = type_name<E>();
+#ifdef __clang__
+    std::string_view name(__PRETTY_FUNCTION__);
+    constexpr std::string_view prefix("auto npp::enum_name() [E = ");
+    constexpr std::string_view infix("; E Val = ");
+    constexpr std::string_view suffix("]");
+#elif defined(__GNUC__)
+    std::string_view name(__PRETTY_FUNCTION__);
+    constexpr std::string_view prefix("constexpr auto npp::enum_name() [with E = ");
+    constexpr std::string_view infix("; E Val = ");
+    constexpr std::string_view suffix("]");
+#elif defined(_MSC_VER)
+    std::string_view name(__FUNCSIG__);
+    constexpr std::string_view prefix("auto __cdecl npp::enum_name<");
+    constexpr std::string_view infix(", ");
+    constexpr std::string_view suffix(">(void)");
+#else
+#error "Unsupported compiler"
+#endif
+    name.remove_prefix(prefix.size() + enum_type_name.size() + infix.size() + enum_type_name.size() + 2 /* "::" */);
     name.remove_suffix(suffix.size());
     return name;
 }
--- test/type_name.cc
+++ test/type_name.cc
@@ -5,6 +5,14 @@
 
 #include <npp/type_name.hh>
 
+#include <fmt/format.h>
+
+enum class test_fruits {
+    strawberry,
+    raspberry,
+    cranberry
+};
+
 
 TEST_CASE("Basic type name") {
     CHECK(npp::type_name<int>() == "int");
@@ -12,4 +20,12 @@ TEST_CASE("Basic type name") {
 
 TEST_CASE("Complex type name") {
     CHECK(npp::type_name<std::vector<int>>() == "std::vector<int>");
+}
+
+TEST_CASE("Enum type name") {
+    CHECK(npp::type_name<test_fruits>() == "test_fruits");
+}
+
+TEST_CASE("Enum value type name") {
+    CHECK(npp::enum_name<test_fruits, test_fruits::strawberry>() == "strawberry");
 }
