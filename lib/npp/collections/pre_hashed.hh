b9a11b465ff766e0d52add8f9ae2dc25df062c8c
NathanPERIER <periernathan2000@gmail.com>
2024-09-22 21:05:11 +0200

    Moved the comparison method out of the pre-hashed object so it can be configured in the map

--- lib/npp/collections/pre_hashed.hh
+++ lib/npp/collections/pre_hashed.hh
@@ -12,28 +12,24 @@
 
 namespace npp {
 
-template <typename T>
-requires (npp::hashable<T> && std::equality_comparable<T>)
+template <npp::hashable Hashable>
 class pre_hashed {
 
 public:
     template <typename... Args>
-    static pre_hashed<T> make(Args&&... args) {
-        return pre_hashed<T>(T(std::forward<Args>(args)...));
+    static pre_hashed<Hashable> make(Args&&... args) {
+        return pre_hashed<Hashable>(Hashable(std::forward<Args>(args)...));
     }
 
-    const T& value() const { return _value; }
+    const Hashable& value() const { return _value; }
     size_t hash() const { return _hash; }
-
-    bool operator==(const pre_hashed<T>& hashed) const { return _value == hashed._value; }
-    bool operator==(const T& value)              const { return _value == value;         }
 
 
 private:
-    T _value;
+    Hashable _value;
     size_t _hash;
 
-    pre_hashed(T&& value): _value(std::forward<T>(value)), _hash(std::hash<T>{}(_value)) {}
+    pre_hashed(Hashable&& value): _value(std::forward<Hashable>(value)), _hash(std::hash<Hashable>{}(_value)) {}
 };
 
 
@@ -58,16 +54,16 @@ struct pre_hash_helper {
 };
 
 
-template <npp::hashable Hashable>
+template <npp::hashable Hashable, class KeyEqual>
 struct pre_hash_eq_helper {
     bool operator()(const npp::pre_hashed<Hashable>& h1, const npp::pre_hashed<Hashable>& h2) const {
-        return h1 == h2;
+        return KeyEqual{}(h1.value(), h2.value());
     }
     bool operator()(const npp::pre_hashed<Hashable>& h, const Hashable& v) const {
-        return h == v;
+        return KeyEqual{}(h.value(), v);
     }
     bool operator()(const Hashable& v, const npp::pre_hashed<Hashable>& h) const {
-        return h == v;
+        return KeyEqual{}(h.value(), v);
     }
 };
 
@@ -76,8 +72,8 @@ struct pre_hash_eq_helper {
 
 namespace npp {
 
-template <hashable Hashable, class T>
-using prehash_umap = std::unordered_map<npp::pre_hashed<Hashable>, T, npp::detail::pre_hash_helper<Hashable>, npp::detail::pre_hash_eq_helper<Hashable>>;
+template <hashable Hashable, class T, class KeyEqual=std::equal_to<Hashable>>
+using prehash_umap = std::unordered_map<npp::pre_hashed<Hashable>, T, npp::detail::pre_hash_helper<Hashable>, npp::detail::pre_hash_eq_helper<Hashable, KeyEqual>>;
 
 } // namespace npp
 
--- test/collections/pre_hashed.cc
+++ test/collections/pre_hashed.cc
@@ -8,6 +8,24 @@
 // Warning: due to compilation issues, this is currently not tested
 
 
+struct cool_thing {
+    std::string name;
+};
+
+template <>
+struct std::hash<cool_thing> {
+    size_t operator()(const cool_thing& thing) {
+        return std::hash<std::string>{}(thing.name);
+    }
+};
+
+struct cool_thing_equal {
+    bool operator()(const cool_thing& o1, const cool_thing& o2) const {
+        return o1.name == o2.name;
+    }
+};
+
+
 TEST_CASE("Pre-hashed map indexed by key type") {
     npp::prehash_umap<std::string, int> map;
     map.insert({ npp::pre_hashed<std::string>::make("hello"),   1 });
@@ -30,5 +48,17 @@ TEST_CASE("Pre-hashed map indexed by hash") {
     REQUIRE(it != map.end());
     CHECK(std::string_view("bonjour") == it->first.value());
     CHECK(hash == it->first.hash());
+    CHECK(1 == it->second);
+}
+
+TEST_CASE("Pre-hashed map with custom equality operator") {
+    npp::prehash_umap<cool_thing, int, cool_thing_equal> map;
+    map.insert({ npp::pre_hashed<cool_thing>::make("pony"),  1 });
+    map.insert({ npp::pre_hashed<cool_thing>::make("train"), 2 });
+    map.insert({ npp::pre_hashed<cool_thing>::make("ice"),   3 });
+    const cool_thing key{"train"};
+    const auto it = map.find(key);
+    REQUIRE(it != map.end());
+    CHECK(key == it->first.value());
     CHECK(1 == it->second);
 }
