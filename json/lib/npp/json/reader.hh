266b39a54d1fd4de2cc2e37c27272054ebd10797
NathanPERIER <periernathan2000@gmail.com>
2024-10-27 02:50:11 +0200

    Merged all read cases in a single method

--- json/lib/npp/json/reader.hh
+++ json/lib/npp/json/reader.hh
@@ -16,37 +16,40 @@ public:
 
     template <json_convertible<Json> JsonConvertible>
     basic_json_reader<Json>& read(std::string_view key, JsonConvertible& value) {
-        const auto it = _data.find(key);
-        if(it == _data.end()) {
-            throw std::runtime_error(fmt::format("Key {} not found", key));
-        }
-        value = npp::convert_json<JsonConvertible>(*it);
+        read_internal<JsonConvertible, false, false>(key, value, value);
         return *this;
     }
 
     template <json_convertible<Json> JsonConvertible>
     basic_json_reader<Json>& read_opt(std::string_view key, JsonConvertible& value) {
-        const auto it = _data.find(key);
-        if(it != _data.end()) {
-            value = npp::convert_json<JsonConvertible>(*it);
-        }
+        read_internal<JsonConvertible, true, false>(key, value, value);
         return *this;
     }
 
-    template <json_convertible<Json> JsonConvertible>
+    template <json_convertible<Json> JsonConvertible> requires(std::copy_constructible<JsonConvertible>)
     basic_json_reader<Json>& read_opt(std::string_view key, JsonConvertible& value, const JsonConvertible& default_value) {
-        const auto it = _data.find(key);
-        if(it != _data.end()) {
-            value = npp::convert_json<JsonConvertible>(*it);
-        } else {
-            value = default_value;
-        }
+        read_internal<JsonConvertible, true, true>(key, value, default_value);
         return *this;
     }
 
 
 private:
     const Json& _data;
+
+    template <json_convertible<Json> JsonConvertible, bool Optional, bool UseDefault>
+    void read_internal(std::string_view key, JsonConvertible& value, const JsonConvertible& default_value) {
+        const auto it = _data.find(key);
+        if(it == _data.end()) {
+            if constexpr (!Optional) {
+                throw std::runtime_error(fmt::format("Key {} not found", key));
+            }
+            if constexpr (UseDefault) {
+                value = default_value;
+            }
+            return;
+        }
+        value = npp::convert_json<JsonConvertible>(*it);
+    }
 
 };
 
