67bbd9c5168e9c6fce115c980c23d626b1af5dd5
NathanPERIER <periernathan2000@gmail.com>
2024-10-27 02:23:20 +0200

    Added converter for stdlib durations

--- json/lib/npp/json/convert_std.hh
+++ json/lib/npp/json/convert_std.hh
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <chrono>
 #include <filesystem>
 
 #include "npp/json/convert_base.hh"
@@ -17,5 +18,16 @@ struct json_converter<std::filesystem::path> {
     }
 };
 
+
+template <class Rep, std::intmax_t Num, std::intmax_t Denom>
+struct json_converter<std::chrono::duration<Rep, std::ratio<Num,Denom>>> {
+
+    using duration_type = std::chrono::duration<Rep, std::ratio<Num,Denom>>;
+
+    template <npp::json Json> requires(json_convertible<Rep, Json>)
+    static duration_type convert(const Json& data) {
+        return duration_type(json_converter<Rep>::convert(data));
+    }
+};
 
 }; // namespace npp
--- json/test/json/convert_addons.cc
+++ json/test/json/convert_addons.cc
@@ -12,3 +12,12 @@ TEST_CASE("Filesystem path JSON conversion") {
     data = nlohmann::json::parse("12");
     CHECK_THROWS(npp::json_converter<std::filesystem::path>::convert(data));
 }
+
+TEST_CASE("Duration JSON conversion") {
+    nlohmann::json data = nlohmann::json::parse("150");
+    std::chrono::milliseconds value = npp::json_converter<std::chrono::milliseconds>::convert(data);
+    CHECK(value == std::chrono::milliseconds(150));
+
+    data = nlohmann::json::parse("true");
+    CHECK_THROWS(npp::json_converter<std::chrono::milliseconds>::convert(data));
+}
