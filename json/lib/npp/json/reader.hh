81e160baffbd0b18a907719aeaba3729bbd67cb0
NathanPERIER <periernathan2000@gmail.com>
2024-10-27 14:01:30 +0100

    Removed the need for dummy lambdas in simple use cases

--- json/lib/npp/json/reader.hh
+++ json/lib/npp/json/reader.hh
@@ -3,9 +3,9 @@
 
 #include <fmt/format.h>
 
-#include "npp/error.hh"
 #include "npp/json/convert_base.hh"
 #include "npp/typing/functional.hh"
+#include "npp/typing/null.hh"
 
 
 namespace npp {
@@ -23,66 +23,65 @@ public:
 
     template <typename T, json_convert_func<Json, T> Conv>
     basic_json_reader<Json>& read(std::string_view key, T& value, Conv converter) {
-        read_internal<T, false, false, Conv>(key, value, std::move(converter), []() -> T { throw unreachable(); });
+        read_internal<T, false, Conv, std::nullptr_t>(key, value, std::move(converter), nullptr);
         return *this;
     }
 
-    template <typename T, json_convert_func<Json, T> Conv>
-    basic_json_reader<Json>& read_opt(std::string_view key, T& value, Conv converter) {
-        read_internal<T, true, false, Conv>(key, value, std::move(converter), []() -> T { throw unreachable(); });
+    template <typename T, json_convert_func<Json, T> Conv, typename DefaultProvider = nullptr_t>
+    requires(npp::provider<DefaultProvider, T> || npp::nulltype<DefaultProvider>)
+    basic_json_reader<Json>& read_opt(std::string_view key, T& value, Conv converter, DefaultProvider get_default = nullptr) {
+        read_internal<T, true, Conv, std::nullptr_t>(key, value, std::move(converter), std::move(get_default));
         return *this;
     }
 
     template <typename T, json_convert_func<Json, T> Conv> requires(std::copy_constructible<T>)
     basic_json_reader<Json>& read_opt(std::string_view key, T& value, Conv converter, const T& default_value) {
-        read_internal<T, true, true, Conv>(key, value, std::move(converter), [&default_value]() -> T { return default_value; });
-        return *this;
-    }
-
-    template <typename T, json_convert_func<Json, T> Conv, npp::provider<T> Provider>
-    basic_json_reader<Json>& read_opt(std::string_view key, T& value, Conv converter, Provider get_default) {
-        read_internal<T, true, true, Conv, Provider>(key, value, std::move(converter), std::move(get_default));
+        read_internal<T, true, Conv>(key, value, std::move(converter), [&default_value]() -> T { return default_value; });
         return *this;
     }
 
 
-    template <json_convertible<Json> JsonConvertible>
-    basic_json_reader<Json>& read(std::string_view key, JsonConvertible& value) {
-        return read<JsonConvertible>(key, value, [](const Json& data) -> JsonConvertible { return npp::convert_json<JsonConvertible>(data); });
+    template <json_convertible<Json> T>
+    basic_json_reader<Json>& read(std::string_view key, T& value) {
+        read_internal<T, false, std::nullptr_t, std::nullptr_t>(key, value, nullptr, nullptr);
+        return *this;
     }
 
-    template <json_convertible<Json> JsonConvertible>
-    basic_json_reader<Json>& read_opt(std::string_view key, JsonConvertible& value) {
-        return read_opt<JsonConvertible>(key, value, [](const Json& data) -> JsonConvertible { return npp::convert_json<JsonConvertible>(data); });
+    template <json_convertible<Json> T, typename DefaultProvider = nullptr_t>
+    requires(npp::provider<DefaultProvider, T> || npp::nulltype<DefaultProvider>)
+    basic_json_reader<Json>& read_opt(std::string_view key, T& value, DefaultProvider get_default = nullptr) {
+        read_internal<T, true, std::nullptr_t, DefaultProvider>(key, value, nullptr, std::move(get_default));
+        return *this;
     }
 
-    template <json_convertible<Json> JsonConvertible> requires(std::copy_constructible<JsonConvertible>)
-    basic_json_reader<Json>& read_opt(std::string_view key, JsonConvertible& value, const JsonConvertible& default_value) {
-        return read_opt<JsonConvertible>(key, value, [](const Json& data) -> JsonConvertible { return npp::convert_json<JsonConvertible>(data); }, default_value);
-    }
-
-    template <json_convertible<Json> JsonConvertible, npp::provider<JsonConvertible> Provider>
-    basic_json_reader<Json>& read_opt(std::string_view key, JsonConvertible& value, Provider get_default) {
-        return read_opt<JsonConvertible>(key, value, [](const Json& data) -> JsonConvertible { return npp::convert_json<JsonConvertible>(data); }, std::move(get_default));
+    template <json_convertible<Json> T> requires(std::copy_constructible<T>)
+    basic_json_reader<Json>& read_opt(std::string_view key, T& value, const T& default_value) {
+        read_internal<T, true, std::nullptr_t>(key, value, nullptr, [&default_value]() -> T { return default_value; });
+        return *this;
     }
 
 
 private:
     const Json& _data;
 
-    template <typename T, bool Optional, bool UseDefault, json_convert_func<Json, T> Conv, npp::provider<T> Provider>
-    void read_internal(std::string_view key, T& value, Conv converter, Provider get_default) {
+    template <typename T, bool Optional, typename Conv, typename DefaultProvider>
+    requires((json_convert_func<Conv, Json, T> || (npp::nulltype<Conv> && json_convertible<T, Json>)) && (npp::provider<DefaultProvider, T> || npp::nulltype<DefaultProvider>))
+    void read_internal(std::string_view key, T& value, Conv converter, DefaultProvider get_default) {
         const auto it = _data.find(key);
         if(it == _data.end()) {
             if constexpr (!Optional) {
                 throw std::runtime_error(fmt::format("Key {} not found", key));
             }
-            if constexpr (UseDefault) {
+            if constexpr (!npp::nulltype<DefaultProvider>) {
                 value = get_default();
             }
             return;
         }
-        value = converter(*it);
+        if constexpr (npp::nulltype<Conv>) {
+            value = npp::convert_json<T>(*it);
+        } else {
+            value = converter(*it);
+        }
     }
 
 };
