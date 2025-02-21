b1e0b757abc3df73417f75dad8e5b4d4c5c5fc19
NathanPERIER <periernathan2000@gmail.com>
2025-02-21 02:00:38 +0100

    Added formatter to print string as JSON

--- core/lib/npp/format/json_string.hh
+++ core/lib/npp/format/json_string.hh
@@ -3,6 +3,7 @@
 
 #include <tuple>
 
+#include "npp/math/bitwise.hh"
 #include "npp/string/lazy.hh"
 
 
@@ -12,4 +13,61 @@ std::string parse_json_string(std::string_view buf, size_t pos = 0);
 
 npp::lazy_string parse_json_string_lazy(std::string_view buf, size_t pos = 0);
 
+
+struct json_string {
+    std::string_view sv;
+};
+
 } // namespace npp
+
+
+template <>
+struct fmt::formatter<npp::json_string> {
+
+    constexpr auto parse(fmt::format_parse_context& ctx) -> fmt::format_parse_context::iterator {
+        auto it = ctx.begin();
+        if (it != ctx.end() && *it != '}') {
+            throw fmt::format_error("invalid json string format");
+        }
+        return it;
+    }
+
+    auto format(npp::json_string str, fmt::format_context& ctx) const -> fmt::format_context::iterator {
+        fmt::format_to(ctx.out(), "\"");
+        size_t begin = 0;
+        size_t pos = 0;
+        while(pos < str.sv.size()) {
+            const char current_char = str.sv[pos];
+            if(current_char == '"' || current_char == '\\') {
+                fmt::format_to(ctx.out(), "{}\\", str.sv.substr(begin, pos - begin));
+                begin = pos;
+                pos++;
+                continue;
+            }
+            if(std::isprint(static_cast<unsigned char>(current_char))) {
+                pos++;
+                continue;
+            }
+            const uint8_t leading_ones = 7 - npp::log2(~static_cast<uint8_t>(current_char));
+            if(leading_ones > 0) {
+                pos += leading_ones;
+                continue;
+            }
+            if(pos > begin) {
+                fmt::format_to(ctx.out(), "{}", str.sv.substr(begin, pos - begin));
+            }
+            switch(current_char) {
+                case '\n': fmt::format_to(ctx.out(), "\\n"); break;
+                case '\t': fmt::format_to(ctx.out(), "\\t"); break;
+                case '\r': fmt::format_to(ctx.out(), "\\r"); break;
+                default:   fmt::format_to(ctx.out(), "\\u00{:0>2x}", current_char);
+            }
+            pos++;
+            begin = pos;
+        }
+        if(begin < str.sv.size() && pos > begin) {
+            fmt::format_to(ctx.out(), "{}", str.sv.substr(begin));
+        }
+        return fmt::format_to(ctx.out(), "\"");
+    }
+};
--- core/test/format/json_string.cc
+++ core/test/format/json_string.cc
@@ -4,19 +4,19 @@
 #include <npp/format/json_string.hh>
 
 
-TEST_CASE("Empty string") {
+TEST_CASE("Parse empty string") {
     CHECK("" == npp::parse_json_string(R"__("")__"));
 }
 
-TEST_CASE("Simple unescaped string") {
+TEST_CASE("Parse simple string without escape sequences") {
     CHECK("a bc" == npp::parse_json_string(R"__("a bc")__"));
 }
 
-TEST_CASE("Non-ASCII string") {
+TEST_CASE("Parse non-ASCII string") {
     CHECK("É" == npp::parse_json_string(R"__("É")__"));
 }
 
-TEST_CASE("Simple escape sequences") {
+TEST_CASE("Parse simple escape sequences") {
     CHECK("\n" == npp::parse_json_string(R"__("\n")__"));
     CHECK("\t" == npp::parse_json_string(R"__("\t")__"));
     CHECK("\r" == npp::parse_json_string(R"__("\r")__"));
@@ -24,11 +24,11 @@ TEST_CASE("Simple escape sequences") {
     CHECK("/"  == npp::parse_json_string(R"__("\/")__"));
 }
 
-TEST_CASE("Escape sequences mixed with normal text") {
+TEST_CASE("Parse escape sequences mixed with normal text") {
     CHECK("a\nb\n\nc" == npp::parse_json_string(R"__("a\nb\n\nc")__"));
 }
 
-TEST_CASE("Simple UTF-16 escape sequences") {
+TEST_CASE("Parse simple UTF-16 escape sequences") {
     CHECK(std::string({ '\0' }) == npp::parse_json_string(R"__("\u0000")__"));
     CHECK("F" == npp::parse_json_string(R"__("\u0046")__"));
     CHECK("É" == npp::parse_json_string(R"__("\u00c9")__"));
@@ -37,7 +37,7 @@ TEST_CASE("Simple UTF-16 escape sequences") {
     CHECK("ン" == npp::parse_json_string(R"__("\u30f3")__"));
 }
 
-TEST_CASE("UTF-16 surrogate escape sequences") {
+TEST_CASE("Parse UTF-16 surrogate escape sequences") {
     CHECK("🐝" == npp::parse_json_string(R"__("\ud83d\udc1d")__"));
 }
 
@@ -46,17 +46,17 @@ TEST_CASE("Parsing out of buffer") {
     CHECK_THROWS(npp::parse_json_string(R"__("aaa")__", 25));
 }
 
-TEST_CASE("Not starting with quote") {
+TEST_CASE("Parse sequence not starting with quote") {
     CHECK_THROWS(npp::parse_json_string(R"__("aaa")__", 1));
 }
 
-TEST_CASE("Unfinished strings") {
+TEST_CASE("Parse unfinished strings") {
     CHECK_THROWS(npp::parse_json_string(R"__(")__"));
     CHECK_THROWS(npp::parse_json_string(R"__("aaa)__"));
     CHECK_THROWS(npp::parse_json_string(R"__("aaa\")__"));
 }
 
-TEST_CASE("Illegal characters") {
+TEST_CASE("Parse illegal characters") {
     CHECK_THROWS(npp::parse_json_string("\"\n\""));
     CHECK_THROWS(npp::parse_json_string("\"\t\""));
     CHECK_THROWS(npp::parse_json_string("\"\r\""));
@@ -65,7 +65,7 @@ TEST_CASE("Illegal characters") {
     CHECK_THROWS(npp::parse_json_string("\"\37\""));
 }
 
-TEST_CASE("Bad UTF-16 escape sequences") {
+TEST_CASE("Parse bad UTF-16 escape sequences") {
     CHECK_THROWS(npp::parse_json_string(R"__("\u)__"));
     CHECK_THROWS(npp::parse_json_string(R"__("\u1)__"));
     CHECK_THROWS(npp::parse_json_string(R"__("\u12)__"));
@@ -78,11 +78,49 @@ TEST_CASE("Bad UTF-16 escape sequences") {
     CHECK_THROWS(npp::parse_json_string(R"__("\uabfg")__"));
 }
 
-TEST_CASE("Bad UTF-16 surrogates") {
+TEST_CASE("Parse bad UTF-16 surrogates") {
     CHECK_THROWS(npp::parse_json_string(R"__("\ud8aa)__"));
     CHECK_THROWS(npp::parse_json_string(R"__("\ud8aa")__"));
     CHECK_THROWS(npp::parse_json_string(R"__("\ud8aaff")__"));
     CHECK_THROWS(npp::parse_json_string(R"__("\udcaa")__"));
     CHECK_THROWS(npp::parse_json_string(R"__("\udcaa\ud8aa")__"));
     CHECK_THROWS(npp::parse_json_string(R"__("\ud8aa\u0102")__"));
+}
+
+
+TEST_CASE("Format empty string") {
+    CHECK(R"__("")__" == fmt::format("{}", npp::json_string{}));
+}
+
+TEST_CASE("Format simple string") {
+    CHECK(R"__("aaa")__" == fmt::format("{}", npp::json_string{"aaa"}));
+}
+
+TEST_CASE("Format only simple escape sequences") {
+    CHECK(R"__("\n")__" == fmt::format("{}", npp::json_string{"\n"}));
+    CHECK(R"__("\t")__" == fmt::format("{}", npp::json_string{"\t"}));
+    CHECK(R"__("\r")__" == fmt::format("{}", npp::json_string{"\r"}));
+    CHECK(R"__("\"")__" == fmt::format("{}", npp::json_string{"\""}));
+    CHECK(R"__("\\")__" == fmt::format("{}", npp::json_string{"\\"}));
+    CHECK(R"__("\n\t\r")__" == fmt::format("{}", npp::json_string{"\n\t\r"}));
+    CHECK(R"__("\"\"")__" == fmt::format("{}", npp::json_string{"\"\""}));
+}
+
+TEST_CASE("Format complex UTF-8 sequences") {
+    const std::string null_str({ '\0' });
+    CHECK(R"__("\u0000")__" == fmt::format("{}", npp::json_string{null_str}));
+    CHECK(R"__("\u0007")__" == fmt::format("{}", npp::json_string{"\x07"}));
+    CHECK(R"__("\u007f")__" == fmt::format("{}", npp::json_string{"\x7f"}));
+    CHECK(R"__("Æ")__" == fmt::format("{}", npp::json_string{"Æ"}));
+    CHECK(R"__("λ")__" == fmt::format("{}", npp::json_string{"λ"}));
+    CHECK(R"__("Щ")__" == fmt::format("{}", npp::json_string{"Щ"}));
+    CHECK(R"__("ホ")__" == fmt::format("{}", npp::json_string{"ホ"}));
+    CHECK(R"__("⛺")__" == fmt::format("{}", npp::json_string{"⛺"}));
+}
+
+TEST_CASE("Format mixed text") {
+    CHECK(R"__("Œuf à la coque")__" == fmt::format("{}", npp::json_string{"Œuf à la coque"}));
+    CHECK(R"__("{\"a\": true, \"b\": false}")__" == fmt::format("{}", npp::json_string{R"__({"a": true, "b": false})__"}));
+    CHECK(R"__("アルゴリズム")__" == fmt::format("{}", npp::json_string{"アルゴリズム"}));
+    CHECK(R"__("Au revoir\n")__" == fmt::format("{}", npp::json_string{"Au revoir\n"}));
 }
