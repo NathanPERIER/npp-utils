d448f405a4c6fc54aad2d23dd9a127b6fdd1e869
NathanPERIER <periernathan2000@gmail.com>
2025-03-25 01:35:35 +0100

    Added observable values

---
+++ core/lib/npp/collections/observable.hh
@@ -0,0 +1,153 @@
+
+#pragma once
+
+#include <concepts>
+#include <functional>
+#include <memory>
+#include <unordered_map>
+
+
+
+namespace npp::detail {
+
+template <typename T>
+requires (std::movable<T> && std::copyable<T>)
+class callback_holder {
+
+public:
+    callback_holder() {}
+
+    callback_holder(callback_holder<T>&&) = delete;
+    callback_holder<T>& operator=(callback_holder<T>&&) = delete;
+    
+    callback_holder(const callback_holder<T>&) = delete;
+    callback_holder<T>& operator=(const callback_holder<T>&) = delete;
+
+    using callback_type = std::function<void(const T&)>;
+
+    std::size_t add_callback(callback_type&& cb) {
+        _prev_id++;
+        _callbacks.insert({_prev_id, std::forward<callback_type>(cb)});
+        return _prev_id;
+    }
+
+    void disarm(std::size_t id) {
+        const auto it = _callbacks.find(id);
+        if(it == _callbacks.end()) {
+            return;
+        }
+        _callbacks.erase(it);
+    }
+
+    void trigger_update(const T& value) {
+        for(const auto& e: _callbacks) {
+            e.second(value);
+        }
+    }
+
+private:
+    std::size_t _prev_id = 0;
+    std::unordered_map<std::size_t, callback_type> _callbacks;
+
+};
+
+} // namespace npp::detail
+
+namespace npp {
+
+template <typename T>
+requires (std::movable<T> && std::copyable<T>)
+class [[nodiscard]] subscription {
+    
+public:
+    subscription(): _holder(nullptr) {}
+    subscription(std::shared_ptr<npp::detail::callback_holder<T>> holder, std::size_t id): _holder(holder), _id(id) {}
+
+    subscription(subscription<T>&&) = default;
+    subscription<T>& operator=(subscription<T>&&) = default;
+    
+    subscription(const subscription<T>&) = delete;
+    subscription<T>& operator=(const subscription<T>&) = delete;
+
+    bool is_active() const { return _holder != nullptr; }
+
+    void unsubscribe() {
+        if(_holder == nullptr) {
+            return;
+        }
+        _holder->disarm(_id);
+        _holder = nullptr;
+    }
+
+    ~subscription() {
+        unsubscribe();
+    }
+
+private:
+    std::shared_ptr<npp::detail::callback_holder<T>> _holder;
+    std::size_t _id;
+
+};
+
+
+template <typename T>
+requires (std::movable<T> && std::copyable<T>)
+class observable {
+
+public:
+    observable(T&& value): _value(std::make_shared<T>(std::forward<T>(value))), _holder(std::make_shared<npp::detail::callback_holder<T>>()) {}
+
+    const T& operator*() const { return *_value; }
+    const T* operator->() const { return _value.get(); }
+
+    template <typename... Args>
+    requires (std::constructible_from<T, Args...>)
+    static observable<T> make(Args&&... args) {
+        return observable<T>(T(std::forward<Args>(args)...));
+    }
+
+
+    std::shared_ptr<const T> get() const { return _value; }
+
+    std::shared_ptr<T> working_copy() {
+        if(_working_copy == nullptr) {
+            _working_copy = std::make_shared<T>(*_value);
+        }
+        return _working_copy;
+    }
+
+    void abort_changes() {
+        _working_copy = nullptr;
+    }
+
+    void commit_changes() {
+        if(_working_copy == nullptr) {
+            return;
+        }
+        *_value = std::move(*_working_copy);
+        _working_copy = nullptr;
+        _holder->trigger_update(*_value);
+    }
+
+    void set(T&& value) {
+        *_value = std::forward<T>(value);
+        _holder->trigger_update(*_value);
+    }
+
+    subscription<T> subscribe(npp::detail::callback_holder<T>::callback_type callback, bool call_now = false) {
+        if(call_now) {
+            callback(*_value);
+        }
+        const std::size_t id = _holder->add_callback(std::move(callback));
+        return subscription<T>(_holder, id);
+    }
+
+private:
+    std::shared_ptr<T> _value;
+    std::shared_ptr<T> _working_copy = nullptr;
+    std::shared_ptr<npp::detail::callback_holder<T>> _holder;
+
+};
+
+} // namespace npp
+
---
+++ core/test/collections/observable.cc
@@ -0,0 +1,128 @@
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <string>
+
+#include <npp/collections/observable.hh>
+
+
+TEST_CASE("Lambda called when value changes") {
+    npp::observable<uint64_t> obs(1);
+    std::size_t times_called = 0;
+    const npp::subscription sub1 = obs.subscribe([&times_called](const uint64_t& val) {
+        times_called++;
+        CHECK(val == 2);
+    });
+    CHECK(sub1.is_active());
+    obs.set(2);
+    CHECK(*obs == 2);
+    CHECK(times_called == 1);
+}
+
+TEST_CASE("Call at subscription") {
+    npp::observable<uint64_t> obs(1);
+    std::size_t times_called = 0;
+    const npp::subscription sub1 = obs.subscribe([&times_called](const uint64_t& val) {
+        times_called++;
+        CHECK(val == times_called);
+    }, true);
+    CHECK(sub1.is_active());
+    obs.set(2);
+    CHECK(times_called == 2);
+}
+
+TEST_CASE("Implicit unsubscription") {
+    npp::observable<uint64_t> obs(1);
+    std::size_t times_called_1 = 0;
+    std::size_t times_called_2 = 0;
+
+    const npp::subscription sub1 = obs.subscribe([&times_called_1](const uint64_t& val) {
+        times_called_1++;
+        CHECK(val == (times_called_1+1));
+    });
+    CHECK(sub1.is_active());
+    obs.set(2);
+
+    {
+        npp::subscription sub2 = obs.subscribe([&times_called_2](const uint64_t& val) {
+            times_called_2++;
+            CHECK(val == (times_called_2+2));
+        });
+        CHECK(sub2.is_active());
+        obs.set(3);
+    }
+
+    CHECK(sub1.is_active());
+    obs.set(4);
+
+    CHECK(times_called_1 == 3);
+    CHECK(times_called_2 == 1);
+}
+
+TEST_CASE("Explicit unsubscription") {
+    npp::observable<uint64_t> obs(1);
+    std::size_t times_called_1 = 0;
+    std::size_t times_called_2 = 0;
+
+    const npp::subscription sub1 = obs.subscribe([&times_called_1](const uint64_t& val) {
+        times_called_1++;
+        CHECK(val == (times_called_1+1));
+    });
+    CHECK(sub1.is_active());
+    obs.set(2);
+
+    npp::subscription sub2 = obs.subscribe([&times_called_2](const uint64_t& val) {
+        times_called_2++;
+        CHECK(val == (times_called_2+2));
+    });
+    CHECK(sub2.is_active());
+    obs.set(3);
+
+    sub2.unsubscribe();
+    CHECK(sub1.is_active());
+    CHECK(!sub2.is_active());
+    obs.set(4);
+
+    sub2.unsubscribe();
+    CHECK(sub1.is_active());
+    CHECK(!sub2.is_active());
+    obs.set(5);
+
+    CHECK(times_called_1 == 4);
+    CHECK(times_called_2 == 1);
+}
+
+TEST_CASE("Edit with transaction") {
+    npp::observable<std::string> obs = npp::observable<std::string>::make("hello");
+    std::size_t times_called = 0;
+    const npp::subscription sub1 = obs.subscribe([&times_called](const std::string& val) {
+        times_called++;
+        CHECK(val == "ifmmp");
+    });
+    CHECK(sub1.is_active());
+    std::shared_ptr<std::string> working = obs.working_copy();
+    for(std::size_t i = 0; i < working->size(); i++) {
+        (*working)[i]++;
+    }
+    CHECK(times_called == 0);
+    CHECK(*obs == "hello");
+    obs.commit_changes();
+    CHECK(times_called == 1);
+}
+
+TEST_CASE("Abort transaction") {
+    std::string_view initial_value = "oh, hi";
+    npp::observable<std::string> obs = npp::observable<std::string>::make(initial_value);
+    const npp::subscription sub1 = obs.subscribe([](const std::string& val) {
+        (void) val;
+        FAIL_CHECK("Listener should never be called");
+    });
+    CHECK(sub1.is_active());
+    std::shared_ptr<std::string> working = obs.working_copy();
+    *working = "goodbye";
+    CHECK(*obs == initial_value);
+    obs.abort_changes();
+    CHECK(*obs == initial_value);
+    obs.commit_changes();
+    CHECK(*obs == initial_value);
+}
