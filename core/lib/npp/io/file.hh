f3be52a9ef473300e8eacf60792f511f77f7d390
NathanPERIER <periernathan2000@gmail.com>
2025-06-11 01:38:35 +0200

    Improved file reading method to allow using any kind of input stream

--- core/lib/npp/io/file.hh
+++ core/lib/npp/io/file.hh
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <cstdint>
+#include <istream>
 #include <span>
 #include <string>
 #include <vector>
@@ -12,7 +13,10 @@ namespace npp {
 void write_to_file(const std::string& filepath, std::string_view text);
 void write_to_file(const std::string& filepath, std::span<const uint8_t> binary);
 
-std::string read_file_text(const std::string& filename);
-std::vector<uint8_t> read_file_binary(const std::string& filename);
+std::string read_istream_text(std::istream& in);
+std::vector<uint8_t> read_istream_binary(std::istream& in);
+
+std::string read_file_text(const std::string& filepath);
+std::vector<uint8_t> read_file_binary(const std::string& filepath);
 
 } // namespace npp
--- core/src/io/file.cc
+++ core/src/io/file.cc
@@ -1,6 +1,7 @@
 #include "npp/io/file.hh"
 
 #include <fstream>
+#include <iterator>
 
 
 namespace npp {
@@ -16,26 +17,29 @@ void write_to_file(const std::string& filepath, std::span<const uint8_t> binary)
 }
 
 
-std::string read_file_text(const std::string& filename) {
-	std::ifstream in(filename, std::ios::in);
-	in.seekg(0, std::ios_base::end);
-	size_t file_size = static_cast<size_t>(in.tellg());
-	std::string res(file_size, '\0');
-	in.seekg(0);
-	in.read(res.data(), file_size);
-	in.close();
-	return res;
+std::string read_istream_text(std::istream& in) {
+	return std::string(
+		std::istreambuf_iterator<char>(in),
+		std::istreambuf_iterator<char>()
+	);
 }
 
-std::vector<uint8_t> read_file_binary(const std::string& filename) {
-	std::ifstream in(filename, std::ios::in | std::ios::binary);
-	in.seekg(0, std::ios_base::end);
-	size_t file_size = static_cast<size_t>(in.tellg());
-	std::vector<uint8_t> res(file_size);
-	in.seekg(0);
-	in.read(reinterpret_cast<char*>(res.data()), file_size);
-	in.close();
-	return res;
+std::vector<uint8_t> read_istream_binary(std::istream& in) {
+	return std::vector<uint8_t>(
+		std::istreambuf_iterator<char>(in),
+		std::istreambuf_iterator<char>()
+	);
+}
+
+
+std::string read_file_text(const std::string& filepath) {
+	std::ifstream in(filepath, std::ios::in);
+	return read_istream_text(in);
+}
+
+std::vector<uint8_t> read_file_binary(const std::string& filepath) {
+	std::ifstream in(filepath, std::ios::in | std::ios::binary);
+	return read_istream_binary(in);
 }
 
 } // namespace npp
